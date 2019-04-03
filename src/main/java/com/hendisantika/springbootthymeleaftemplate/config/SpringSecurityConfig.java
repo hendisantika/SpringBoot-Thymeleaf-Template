483248cbab7317664570d566a1c938766bb95c6b
hendisantika <hendisantika@yahoo.co.id>
2019-04-04 06:56:27 +0700

    #14 - Add configure method in SpringSecurity Config

--- src/main/java/com/hendisantika/springbootthymeleaftemplate/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplate/config/SpringSecurityConfig.java
@@ -3,7 +3,8 @@ package com.hendisantika.springbootthymeleaftemplate.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.sql.DataSource;
@@ -19,7 +20,7 @@ import javax.sql.DataSource;
  */
 
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfiguration {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
     @Autowired
@@ -35,5 +36,40 @@ public class SpringSecurityConfig extends WebSecurityConfiguration {
 
     @Value("${spring.queries.roles-query}")
     private String rolesQuery;
+
+    /**
+     * HTTPSecurity configurer
+     * - roles ADMIN allow to access /admin/**
+     * - roles USER allow to access /user/** and /newPost/**
+     * - anybody can visit /, /home, /about, /registration, /error, /blog/**, /post/**, /h2-console/**
+     * - every other page needs authentication
+     * - custom 403 access denied handler
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/registration", "/error", "/h2-console/**").permitAll()
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+                // Fix for H2 console
+                .and().headers().frameOptions().disable();
+    }
+
 
 }
