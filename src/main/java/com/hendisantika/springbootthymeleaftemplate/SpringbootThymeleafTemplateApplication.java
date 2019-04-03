286abe45840bc8a0478f1965d1666a90c717eedc
hendisantika <hendisantika@yahoo.co.id>
2019-04-04 06:41:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleaftemplate/SpringbootThymeleafTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleaftemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafTemplateApplication.class, args);
+    }
+
+}
