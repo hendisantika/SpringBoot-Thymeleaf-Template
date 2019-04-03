9e9ba75f30c38efabea089729f81d3dad19c14ef
hendisantika <hendisantika@yahoo.co.id>
2019-04-04 06:58:15 +0700

    #16 - Add ErrorController

---
+++ src/main/java/com/hendisantika/springbootthymeleaftemplate/exception/ErrorController.java
@@ -0,0 +1,37 @@
+package com.hendisantika.springbootthymeleaftemplate.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-template
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-04
+ * Time: 06:57
+ */
+@ControllerAdvice
+public class ErrorController {
+
+    private static Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView exception(final Throwable throwable, final Model model) {
+        logger.error("Exception during execution of SpringSecurity application", throwable);
+
+        ModelAndView modelAndView = new ModelAndView("error");
+        String errorMessage = (throwable != null ? throwable.toString() : "Unknown error");
+        modelAndView.addObject("errorMessage", errorMessage);
+        return modelAndView;
+    }
+
+}
