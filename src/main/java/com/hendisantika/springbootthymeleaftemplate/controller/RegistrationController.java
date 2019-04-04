6295641ceec47274cc046b8957a5e20554dcf18b
hendisantika <hendisantika@yahoo.co.id>
2019-04-04 07:00:50 +0700

    #20 - Add createNewUser method in Registration Controller

--- src/main/java/com/hendisantika/springbootthymeleaftemplate/controller/RegistrationController.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplate/controller/RegistrationController.java
@@ -4,8 +4,12 @@ import com.hendisantika.springbootthymeleaftemplate.model.User;
 import com.hendisantika.springbootthymeleaftemplate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,6 +32,35 @@ public class RegistrationController {
         User user = new User();
         modelAndView.addObject("user", user);
         modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @PostMapping("/registration")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userWithEnteredEmailExists = userService.findByEmail(user.getEmail());
+        if (userWithEnteredEmailExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        User userWithEnteredUsernameExists = userService.findByUsername(user.getUsername());
+        if (userWithEnteredUsernameExists != null) {
+            bindingResult
+                    .rejectValue("username", "error.user",
+                            "There is already a user registered with the username provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            // Registration successful, save user
+            // Set user role to USER and set it as active
+            userService.saveUser(user);
+
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+        }
         return modelAndView;
     }
 }
